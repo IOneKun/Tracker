8fc59fdffd58b1213f010b37911b9282594fc08f
IvanKuninets <ivankuninec8@gmail.com>
2025-07-27 09:17:42 +0200

    no message

--- Tracker/ViewControllers/MainTabBarViewController.swift
+++ Tracker/ViewControllers/MainTabBarViewController.swift
@@ -32,7 +32,6 @@ final class MainTabBarViewController: UITabBarController {
             tabBar.standardAppearance = tabBarAppearance
             tabBar.scrollEdgeAppearance = tabBarAppearance
         }
-
     }
 }
 
